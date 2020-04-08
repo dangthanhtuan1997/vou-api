01eca1c78008346fc50dfbf7e3501e8f9e91d530
Đặng Thanh Tuấn <contact.dangthanhtuan@gmail.com>
2020-04-08 13:51:52 +0700

    chatbot test

---
+++ src/routes/chatbot.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../passport/passport');
+const Voucher = require('../model/voucher.model');
+const Partner = require('../model/partner.model');
+const User = require('../model/user.model');
+
+module.exports = (app) => {
+    app.use('/chatbot', router);
+
+    router.post('/', async (req, res) => {
+        return res.status(200).json(req.body);
+    });
+};
