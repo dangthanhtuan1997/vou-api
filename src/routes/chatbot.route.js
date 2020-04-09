4b1ebcb84fcb622e00026270606c462bad925074
Đặng Thanh Tuấn <contact.dangthanhtuan@gmail.com>
2020-04-09 14:47:25 +0700

    test chatbot

--- src/routes/chatbot.route.js
+++ src/routes/chatbot.route.js
@@ -4,12 +4,14 @@ const passport = require('../passport/passport');
 const Voucher = require('../model/voucher.model');
 const Partner = require('../model/partner.model');
 const User = require('../model/user.model');
+const axios = require('axios');
 
 module.exports = (app) => {
     app.use('/chatbot', router);
 
     router.post('/', async (req, res) => {
-        console.log(req.body);
-        return res.status(200).json(req.body);
+        const {fullName, phone} = req.body;
+        console.log(fullName, phone);
+        return res.status(200).json();
     });
 };
