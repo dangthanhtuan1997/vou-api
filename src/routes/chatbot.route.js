4840ee499775389afadca5ed4af55e5696afbd12
Đặng Thanh Tuấn <contact.dangthanhtuan@gmail.com>
2020-04-09 15:18:16 +0700

    test bot

--- src/routes/chatbot.route.js
+++ src/routes/chatbot.route.js
@@ -10,13 +10,14 @@ module.exports = (app) => {
 
     router.post('/', async (req, res) => {
         const { fullName, phone } = req.body;
-
+        
         const response = await axios.post('https://service-api.accesstrade.vn/api/v1/lead/store?campaign_id=61&ad_space_code=ADS000010668&token=ba441b0c86bb92c9ed98bd298c8c2827', {
             name: fullName,
             phone: phone
-        })
-        const chatbot = new ChatBot({ name: fullName, phone: phone, data: JSON.stringify(response) });
-        await chatbot.save();
+        });
+        console.log(response);
+        // const chatbot = new ChatBot({ name: fullName, phone: phone, data: JSON.stringify(response) });
+        // await chatbot.save();
         return res.status(200).end();
     });
 };
